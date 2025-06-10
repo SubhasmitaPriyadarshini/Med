a2c8725aa2953d34a5ef04c8670eb513d4bd6759
Lipsas02 <lipsas1217@gmail.com>
2025-06-10 11:49:07 +0530

    updated

--- constant/Colors.ts
+++ constant/Colors.ts
@@ -1,7 +1,13 @@
 const appColors = {
-   Primary:"#65a30d",
-   White:"#ffffff",
-   Black:"#000000",
+   ButtonLinerGradient1:"#1DAC92",
+   ButtonLinerGradient2:"#1DAC92",
+   ButtonLinerGradient3:"#228E8E",
+   backgorundColorLight:"#ffffff",
+   backgroundColorDark:"#000000",
+   inputfieldDark:"#334155",
+   inputfieldLight:"#F1F5F9",
+   selecteditemLight:"#252D41",
+   selecteditemDark:"#4A5A82",
   };
   
   export default appColors;
---
+++ theme/index.ts
@@ -0,0 +1,7 @@
+// Export all theme-related items from a single file for easier imports
+export * from './themes';
+export * from './ThemeContext';
+export * from './useTheme';
+
+// Export ThemeProvider as default for easier imports
+export { ThemeProvider as default } from './ThemeContext'; 
